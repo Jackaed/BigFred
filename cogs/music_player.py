a307e71505fe829cd82d3af0aa06d6bfdab65430
Jamie Kimber <jamiekimber@btinternet.com>
2021-09-26 20:30:07 +0100

    I AM A GOD

--- cogs/music_player.py
+++ cogs/music_player.py
@@ -5,10 +5,16 @@ import discord
 import os
 from discord.ext import commands
 import youtube_dl
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 import messages
 from cogs.fred_functions import FredFunctions
 
+client_id =  "ab145034fa4e431ea3a94666038f3f1e"
+client_secret = "97a896b65a3444e88a9e713cdee7248b"
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 class MusicPlayer(commands.Cog):
 
@@ -18,11 +24,39 @@ class MusicPlayer(commands.Cog):
 
         self.clients: {int: discord.VoiceClient} = {}
 
+    @commands.command(aliases=["np", "current", "playing"])
+    async def song(self, ctx: commands.Context, *args):
+        if ctx.message.author.voice is None:
+            await self.fred_functions.custom_error(ctx, "User not in VC",
+                                                   "You need to be in a voice channel to use this command.")
+            return
+
+        try:
+            id = str(ctx.guild.id)
+            with open("Songs/info." + id) as f:
+                info = f.readlines()
+
+            embed = discord.Embed(title="Current Song", description=info[0].rstrip("\n"),
+                                  colour=discord.Colour.purple())
+            embed.set_image(url=info[1].rstrip("\n"))
+            embed.set_footer(text=info[2].rstrip("\n"))
+
+            info_field = info[3].rstrip("\n") + "\n" + info[4].rstrip("\n") + "\n" + info[5].rstrip("\n")
+
+            embed.add_field(name="Info", value=info_field)
+
+            discord.Message = await ctx.reply(embed=embed)
+        except FileNotFoundError:
+            discord.Message = await ctx.reply(
+                embed=discord.Embed(title="No Song playing", description="There is currently no song playing.",
+                                    colour=discord.Colour.purple()))
+
     @commands.command(aliases=["p", "resume", "r"])
     async def play(self, ctx: commands.Context, *args):
 
         if ctx.message.author.voice is None:
-            await self.fred_functions.custom_error(ctx, "User not in VC", "You need to be in a voice channel to use this command.")
+            await self.fred_functions.custom_error(ctx, "User not in VC",
+                                                   "You need to be in a voice channel to use this command.")
             return
 
         if ctx.guild.id in self.clients and self.clients[ctx.guild.id].is_paused():
@@ -31,6 +65,15 @@ class MusicPlayer(commands.Cog):
             return
 
         query = " ".join(args) if args else random.choice(messages.SONGS)
+
+        if "https://open.spotify.com/track/" in query:
+            query.strip("https://open.spotify.com/track/")
+            spotify_info = spotify.track(query)
+            for i in spotify_info["artists"]:
+                print(i)
+            query = f'{spotify_info["name"]} by {spotify_info["album"]["artists"][0]["name"]}'
+            print(query)
+
 
         user = ctx.message.author
         voice_channel = user.voice.channel
@@ -47,7 +90,9 @@ class MusicPlayer(commands.Cog):
         if os.path.exists(filename):
             os.remove(filename)
 
-        msg: discord.Message = await ctx.reply(embed=discord.Embed(title="Music Player", description="Downloading song...", colour=discord.Colour.purple()))
+        msg: discord.Message = await ctx.reply(
+            embed=discord.Embed(title="Music Player", description="Downloading song...",
+                                colour=discord.Colour.purple()))
         audio, info = self.get_audio(query, filename)
         voice_client.play(audio)
 
@@ -66,6 +111,17 @@ class MusicPlayer(commands.Cog):
         embed.add_field(name="Info", value=info_field)
 
         await msg.edit(embed=embed)
+
+        os.makedirs("Songs", exist_ok=True)
+        id = str(ctx.guild.id)
+        f = open("Songs/info." + id, "w")
+        f.write(f"Now Playing: `{info.get('track', info['title'])}`" + "\n" +
+                info['thumbnail'] + "\n" +
+                f"https://www.youtube.com/watch?v={info['id']}" + "\n" +
+                f"Album: `{info.get('album', 'none')}`\n" \
+                f"Artist: `{info.get('artist', 'none')}`\n" \
+                f"Duration: `{info.get('duration', -1)} seconds`")
+        f.close()
 
         while voice_client.is_playing() or voice_client.is_paused():
             await asyncio.sleep(1)
@@ -110,4 +166,3 @@ class MusicPlayer(commands.Cog):
 
 def setup(bot: commands.Bot):
     bot.add_cog(MusicPlayer(bot))
-
