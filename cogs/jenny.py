2b0b4524150696aa49644a58cf46f96e1541c010
Jackaed <ed.andy.jackson@gmail.com>
2021-10-13 22:45:43 +0100

    Jenny now properly restarts the cogs if there is an update
    fr
    no kapp
    no cap
    no kizzler
    on god
    respectfully

--- cogs/jenny.py
+++ cogs/jenny.py
@@ -8,12 +8,16 @@ class Jenny (commands.Cog):
 
     @commands.command()
     async def update(self, ctx: commands.context):
-        if ctx.guild.id == 891429195811545158 and secrets.HOSTER_ID[:-4] == "chen":
-            await asyncio.create_subprocess_shell("git fetch --all")
-            await asyncio.create_subprocess_shell("git reset --hard")
+        if ctx.guild.id == 891429195811545158 and secrets.HOSTER_ID[-4:] == "chen":
             for cog in os.listdir("cogs"):
                 if cog.split(".", 2)[-1] == "py" and cog[0] != "_" and cog != "jenny.py":
-                    ctx.bot.reload_extension("cogs." + cog[:-3])
+                    ctx.bot.unload_extension("cogs." + cog[:-3])
+                    print(f"> Unloaded {cog}")
+            wait = await asyncio.create_subprocess_shell("git fetch --all && git reset --hard")
+            await wait.wait()
+            for cog in os.listdir("cogs"):
+                if cog.split(".", 2)[-1] == "py" and cog[0] != "_" and cog != "jenny.py":
+                    ctx.bot.load_extension("cogs." + cog[:-3])
                     print(f"> Reloaded {cog}")
 
 
