0fc858045541d76d2a3ef12b38fcc1e6d3bf442a
KitDaCatsun <45962810+KitDaCatsun@users.noreply.github.com>
2021-10-13 20:41:27 +0100

    Update jenny.py

--- cogs/jenny.py
+++ cogs/jenny.py
@@ -7,7 +7,7 @@ class Jenny (commands.Cog):
 
     @commands.command()
     async def update(self, ctx: commands.context):
-        if ctx.guild.id == 891429195811545158 and secrets.HOSTER_ID[:-5] == "Aachen":
+        if ctx.guild.id == 891429195811545158 and secrets.HOSTER_ID[:-4] == "chen":
             os.system("git fetch --all")
             os.system("git reset --hard")
 
